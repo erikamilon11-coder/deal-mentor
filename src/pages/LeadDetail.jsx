92bbc2f2dd7b9d1fbd303601a0069a8e915cfcff
Erika Milon <bluestoneholdings123@gmail.com>
2026-03-12 00:49:23 +0000

    File changes

--- src/pages/LeadDetail.jsx
+++ src/pages/LeadDetail.jsx
@@ -425,6 +425,7 @@ export default function LeadDetail() {
                 leadId={leadId}
                 owners={owners}
                 phones={phones}
+                lead={lead}
               />
             </TabsContent>
 
